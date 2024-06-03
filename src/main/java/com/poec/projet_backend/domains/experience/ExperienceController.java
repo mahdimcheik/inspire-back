ad225f019b406f09898e4e2b6880d285afaef2c3
Hepsox <marie.delaire@outlook.com>
2024-06-03 17:11:04 +0200

    experience

--- src/main/java/com/poec/projet_backend/domains/experience/ExperienceController.java
+++ src/main/java/com/poec/projet_backend/domains/experience/ExperienceController.java
@@ -1,13 +1,23 @@
 package com.poec.projet_backend.domains.experience;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/experience")
+@Data
+@RequestMapping("/experience")
 public class ExperienceController {
 
-    @Autowired
     private ExperienceService service;
+
+    @PostMapping("/add")
+
+    public Experience addExperience (@RequestBody Experience experience){
+        return service.createExperience(experience);
+
+    }
 }
--- src/main/java/com/poec/projet_backend/domains/experience/ExperienceService.java
+++ src/main/java/com/poec/projet_backend/domains/experience/ExperienceService.java
@@ -1,11 +1,17 @@
 package com.poec.projet_backend.domains.experience;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Data
 @Service
 public class ExperienceService {
 
-    @Autowired
     private ExperienceRepository repository;
+
+    public Experience createExperience(Experience experience){
+        return repository.save(experience);
+
+    }
 }
