63cf38b86a0ad56195fc7a583b73a8bf8aa511d6
mahdi mcheik <mahdi.mcheik@hotmail.fr>
2024-06-03 15:05:12 +0200

    creating mentor and student and one to one relationship with UserApp

---
+++ src/main/java/com/poec/projet_backend/mentor/Mentor.java
@@ -0,0 +1,30 @@
+package com.poec.projet_backend.mentor;
+
+import com.poec.projet_backend.user_app.UserApp;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.apache.logging.log4j.message.LoggerNameAwareMessage;
+
+@Entity
+@Data
+@Table(name = "mentor")
+
+public class Mentor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String title;
+    private String description;
+    @Column(name = "imgUrl")
+    private String imgUrl;
+    @Column(name = "githubUrl")
+    private String githubUrl;
+    @Column(name = "linkedinUrl")
+    private String linkedinUrl;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userId")
+    private UserApp user;
+}
