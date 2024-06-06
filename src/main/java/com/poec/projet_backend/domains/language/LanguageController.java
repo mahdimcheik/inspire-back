93668ff61e488d9eee9b247c354e64e75e847dac
Hepsox <marie.delaire@outlook.com>
2024-06-06 11:02:43 +0200

    add getAll languages methode

--- src/main/java/com/poec/projet_backend/domains/language/LanguageController.java
+++ src/main/java/com/poec/projet_backend/domains/language/LanguageController.java
@@ -3,13 +3,21 @@ package com.poec.projet_backend.domains.language;
 import com.poec.projet_backend.domains.formation.FormationService;
 import lombok.Data;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Data
 @RestController
-@RequestMapping("/api/v1/language")
+@RequestMapping("/language")
 public class LanguageController {
 
-    private LanguageService service;
+    private final LanguageService service;
+
+    @GetMapping("/get/all")
+    public List<LanguageDTO> getAllLanguages() {
+        return service.getAllLanguages();
+    }
 }
