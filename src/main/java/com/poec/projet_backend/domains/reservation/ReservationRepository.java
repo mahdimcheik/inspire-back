93f03d6294b32c601b50504225b8fb2971ef2827
Mathieu Chauveau <chauveau.math@gmail.com>
2024-06-11 10:33:11 +0200

    change reservationRepo

--- src/main/java/com/poec/projet_backend/domains/reservation/ReservationRepository.java
+++ src/main/java/com/poec/projet_backend/domains/reservation/ReservationRepository.java
@@ -1,6 +1,7 @@
 package com.poec.projet_backend.domains.reservation;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface ReservationRepository {
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
